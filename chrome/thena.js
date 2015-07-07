361643ccb0e8cf6071c10e5ed807e02b49c136ec
Daniel Kronovet <kronovet@gmail.com>
2015-07-07 19:19:58 -0400

    wrote getAuth to fetch auth token and send auth data to server

--- chrome/thena.js
+++ chrome/thena.js
@@ -10,6 +10,7 @@
 // Show page action icon in omnibar.
 function showPageAction(tabId, changeInfo, tab) {
     chrome.pageAction.show(tabId);
+    // getAuth();
 };
 // Call the above function when the url of a tab changes.
 chrome.tabs.onUpdated.addListener(showPageAction);
@@ -17,11 +18,30 @@ chrome.tabs.onUpdated.addListener(showPageAction);
 // Authenticate User
 function getAuth() {
   chrome.identity.getAuthToken({interactive: true}, function(token) {
-    alert(token)
-  // "https://www.googleapis.com/userinfo/email?alt=json" URL to get email address
+      email_url = "https://www.googleapis.com/userinfo/email?alt=json&access_token="
+
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", email_url + token);
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          var resp = JSON.parse(xhr.responseText);
+          var authData = JSON.stringify({
+            token: token,
+            email: resp.data.email,
+            isVerified: resp.data.isVerified
+          })
+
+          // Send authentication to server
+          var xhr2 = new XMLHttpRequest();
+          xhr2.open("POST", "http://127.0.0.1:5000/login");
+          xhr2.send(authData);
+          alert(authData)
+
+        };
+      };
+      xhr.send();
   });
 };
-
 
 // Save Arcs
 function sendArc(details) {
@@ -37,12 +57,12 @@ function sendArc(details) {
     chrome.identity.getProfileUserInfo(function(userInfo) {
       var email = userInfo.email
 
-      arcData = JSON.stringify({tail: tail, head: head, email: email});
+      var arcData = JSON.stringify({tail: tail, head: head, email: email});
 
       // Send to server
-      var client = new XMLHttpRequest();
-      client.open("POST", "http://127.0.0.1:5000/arc");
-      client.send(arcData);
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://127.0.0.1:5000/arc");
+      xhr.send(arcData);
       alert(arcData);
     });
   });
