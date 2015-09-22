5100823187fba1540e4103d18d58978651fa46a5
Daniel Kronovet <kronovet@gmail.com>
2015-09-22 00:30:26 -0400

    added opsworks-specific resources to deploy recipe

--- infrastructure/chef-repo/cookbooks/thena/recipes/deploy.rb
+++ infrastructure/chef-repo/cookbooks/thena/recipes/deploy.rb
@@ -5,44 +5,40 @@
 # Copyright (c) 2015 Daniel Kronovet, All Rights Reserved.
 
 
-# git 'clone-repo' do
-#   destination node['thena']['project_dir']
-#   ssh_wrapper "#{node['thena']['config_dir']}#{node['thena']['ssh_wrapper']}"
-#   repository node['thena']['git_repo_uri']
-#   # repository node[:deploy]['thena'][:scm][:repository]
-#   revision 'master'
-#   action :sync
-# end
+include_recipe 'deploy'
 
-# include_recipe 'deploy'
+node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
 
-# node[:deploy].each do |application, deploy|
-#   opsworks_deploy_dir do
-#     user deploy[:user]
-#     group deploy[:group]
-#     path deploy[:deploy_to]
-#   end
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
+end
 
-#   opsworks_deploy do
-#     deploy_data deploy
-#     app application
-#   end
-# end
+execute 'update packages' do
+    cwd default['thena']['app_dir']
+    command 'pip install -r requirements.txt'
+end
 
 ## FOR TESTING IN TEST KITCHEN
-python_pip 'uwsgi'
+# python_pip 'uwsgi'
 
-directory node['thena']['app_dir'] do
-  recursive true
-end
+# directory node['thena']['app_dir'] do
+#   recursive true
+# end
 
-file "#{node['thena']['app_dir']}/wsgi.py" do
-    content "def app(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b'Hello Thena\\n']"
-    owner node['thena']['uwsgi_user']
-    group node['thena']['uwsgi_group']
-end
+# file "#{node['thena']['app_dir']}/wsgi.py" do
+#     content "def app(env, start_response):
+#     start_response('200 OK', [('Content-Type','text/html')])
+#     return [b'Hello Thena\\n']"
+#     owner node['thena']['uwsgi_user']
+#     group node['thena']['uwsgi_group']
+# end
 ## / END TESTING
 
 service 'thena-uwsgi' do
